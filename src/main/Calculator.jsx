91731cfeaadc36696af0f8d650d699e6907b20e6
Wesley Lima <w.freitas.lima@gmail.com>
2021-02-27 22:36:14 -0300

    Commit Inicial

---
+++ src/main/Calculator.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../assets/css/calculator.css';
+
+class Calculator extends Component {
+    render() {
+        return (
+            <div className="calculator">
+
+            </div>
+        );
+    }
+}
+
+export default Calculator;
