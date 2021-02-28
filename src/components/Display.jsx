38e2a6efb2852bfab9df865b105d4f3efcff651f
Wesley Lima <w.freitas.lima@gmail.com>
2021-02-28 00:26:43 -0300

    Criado componente display

---
+++ src/components/Display.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import '../assets/css/display.css';
+
+function Component(props) {
+
+    return (
+        <div className="display">
+            {props.defaultValue}
+        </div>
+    );
+}
+
+export default Component;
